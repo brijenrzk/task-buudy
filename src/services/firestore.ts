200374c433b614f8aac30eac28b720ee162f5c65
Brijen Rajak <brijenrajak@gmail.com>
2025-01-09 17:28:49 +0545

    fixed bugs

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,24 +1,7 @@
 // services/firestore.ts
 
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import app from './firebaseConfig';
 
 export const db = getFirestore(app);
 
-export const saveTaskToFirestore = async (task: {
-    title: string;
-    description: string;
-    category: string;
-    dueDate: string;
-    taskStatus: string;
-    fileUrl: string;
-    userId: string;
-}) => {
-    try {
-        const tasksRef = collection(db, 'tasks');
-        const docRef = await addDoc(tasksRef, task);
-        console.log('Task saved with ID:', docRef.id);
-    } catch (error) {
-        console.error('Error saving task:', error);
-    }
-};
